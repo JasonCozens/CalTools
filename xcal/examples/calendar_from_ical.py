af93b9d2b7276b4bc491fa44f1a3ee3de585f4d0
Jason Cozens <jason.cozens@computer.org>
2014-12-21 17:05:52 +0000

    Example based around section 3.4 of RFC 5545.

--- xcal/examples/calendar_from_ical.py
+++ xcal/examples/calendar_from_ical.py
@@ -1,1 +1,49 @@
 __author__ = 'Jason'
+
+import unittest
+import icalendar
+from icalendar import (
+    vText,
+)
+
+
+class Examples5545(unittest.TestCase):
+
+    ical_str = (
+        'BEGIN:VCALENDAR\r\n' +
+        'VERSION:2.0\r\n' +
+        'PRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n' +
+        'BEGIN:VEVENT\r\n' +
+        'UID:19970610T172345Z-AF23B2@example.com\r\n' +
+        'DTSTAMP:19970610T172345Z\r\n' +
+        'DTSTART:19970714T170000Z\r\n' +
+        'DTEND:19970715T040000Z\r\n' +
+        'SUMMARY:Bastille Day Party\r\n' +
+        'END:VEVENT\r\n' +
+        'END:VCALENDAR\r\n'
+    )
+
+    def test_3_4(self):
+        # Arrange.
+        cal = icalendar.Calendar.from_ical(self.ical_str)
+        sub_components = cal.subcomponents
+        # Assert.
+        # 1. type.
+        self.assertEqual(cal.name, 'VCALENDAR')
+        # 2. properties.
+        self.assertEqual(len(cal.items()), 2)
+        self.assertIn('version', cal.keys())
+        self.assertIn('prodid', cal.keys())
+        self.assertEqual(cal['version'], vText('2.0'))
+        self.assertEqual(
+            cal['prodid'],
+            vText('-//hacksw/handcal//NONSGML v1.0//EN'))
+        self.assertEqual(cal.decoded('version'), b'2.0')
+        self.assertEqual(
+            cal.decoded('prodid'),
+            b'-//hacksw/handcal//NONSGML v1.0//EN')
+        # Subcomponents.
+        self.assertEqual(1, len(sub_components))
+        v_event = sub_components[0]
+        self.assertEqual(len(v_event.items()), 5)
+        self.assertEqual(len(v_event.subcomponents), 0)
